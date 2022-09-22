d6d8fc1bc0928c39d0b01e1557284795f3dd12d5
Lidiya <lkarabanova@gmail.com>
2022-09-22 12:15:02 +0300

    task_2 done again

--- task_2/solution.js
+++ task_2/solution.js
@@ -1,7 +1,7 @@
 function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
-    let freeShippingMinSum = 700; // минимальная цена для бесплатной доставки
-    let shippingPrice = 200; // стоимость доставки
+    let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
+    let shippingPrice = shipping; // стоимость доставки
 
     // Задание №2.1. Рассчитать доставку
     let shippingSum;
@@ -30,8 +30,8 @@ function calcShipping(sum, min, shipping) {
 
 function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
-    let discountMinSum = 700; // минимальная цена для скидки
-    let discountPart = 10; // величина скидки в процентах
+    let discountMinSum = min; // минимальная цена для скидки
+    let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
     let discountSum;
